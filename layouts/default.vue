9c9610a67f66abb9f30e676d93c0156355bcdbbd
Kimbo0o <kimkoch1000@gmail.com>
2022-09-25 16:42:46 +0200

    added metadata

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,3 +13,43 @@ body {
   @apply bg-bg;
 }
 </style>
+
+<script lang="ts" setup>
+useHead({
+  title: "Kim Daniel Koch",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/icon-180.png",
+    },
+  ],
+  meta: [
+    {
+      name: "description",
+      content: "Web Developer from Paderborn",
+    },
+    {
+      name: "og:description",
+      content: "Web Developer from Paderborn",
+    },
+    {
+      name: "og:title",
+      content: "Kim Daniel Koch",
+    },
+    {
+      name: "og:url",
+      content: "kimdanielkoch.de",
+    },
+    {
+      name: "og:type",
+      content: "website",
+    },
+  ],
+});
+</script>
